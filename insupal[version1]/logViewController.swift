f0ce0627ce041f088365c8af28b275d5e73e8bc4
Guest User <Guest@d207-023-187-203.wireless.sfu.ca>
2018-07-16 18:58:05 -0700

    withlogin

---
+++ insupal[version1]/logViewController.swift
@@ -0,0 +1,35 @@
+//
+//  logViewController.swift
+//  insupal[version1]
+//
+//  Created by Nicole on 2018-07-02.
+//  Copyright © 2018 cherrycat. All rights reserved.
+//
+
+import UIKit
+
+class logViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
